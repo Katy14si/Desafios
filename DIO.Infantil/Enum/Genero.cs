afe4144638bb843fd86fcf06a52ef6e032c38d90
Katy14si <55769610+Katy14si@users.noreply.github.com>
2021-04-19 17:35:12 -0300

    Add files via upload

--- DIO.Infantil/Enum/Genero.cs
+++ DIO.Infantil/Enum/Genero.cs
@@ -1,14 +1,16 @@
-namespace DIO.Infantil.Enum
+using System;
+
+namespace DIO.Infantil
 {
-   
-        public enum Genero
-	{
-		Aventura = 1,
-		Comedia = 2,
-		Documentario = 3,
-		Fantasia = 4,
-		Musical = 5,
-		
-	}
-    
+
+    public enum Genero
+    {
+        Aventura = 1,
+        Comedia = 2,
+        Documentario = 3,
+        Fantasia = 4,
+        Musical = 5,
+
+    }
+
 }
